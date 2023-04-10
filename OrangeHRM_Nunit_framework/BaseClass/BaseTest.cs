7569ae3ae2fe796706b92a0199d29f59413c31eb
mdmostafamunna <mdmostafamunna.sqa@gmail.com>
2023-04-10 12:33:31 +0600

    Base Class added

---
+++ OrangeHRM_Nunit_framework/BaseClass/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeHRM_Nunit_framework.BaseClass
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+            //driver.Navigate().GoToUrl("");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(6000);
+            driver.Quit();
+        }
+    }
+}
--- OrangeHRM_Nunit_framework/UnitTest1.cs
+++ OrangeHRM_Nunit_framework/UnitTest1.cs
@@ -1,29 +1,17 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OrangeHRM_Nunit_framework.BaseClass;
 
 namespace OrangeHRM_Nunit_framework
 {
     [TestFixture]
 
-    public class Tests
+    public class Tests : BaseTest
     {
-
-        IWebDriver driver;
-
-        [SetUp]
-        public void Setup()
-        {
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-
-            TestContext.Progress.WriteLine("Output for Set up method of  Nunit");
-        }
 
         [Test]
         public void NavigateToTheURL()
         {
-            driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.FindElement(By.Name("username")).SendKeys("Admin");
             driver.FindElement(By.Name("password")).SendKeys("admin123");
             driver.FindElement(By.XPath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).Click();
@@ -39,14 +27,6 @@ namespace OrangeHRM_Nunit_framework
             driver.FindElement(By.XPath("//body/div[@id='app']/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[1]/a[1]")).Click();
 
 
-        }
-
-
-        [TearDown]
-        public void TearDown()
-        {
-            Thread.Sleep(6000);
-            driver.Close();
         }
     }
 }
